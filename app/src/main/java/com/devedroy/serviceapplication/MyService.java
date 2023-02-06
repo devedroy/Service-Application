c9a20f72afb664ba79f113d753a777900c7efd00
Deved Roy <devedroy.dr@gmail.com>
2023-02-07 00:17:17 +0530

    A service shouldn't be stopped explicitly but can be done using stopService method.

--- app/src/main/java/com/devedroy/serviceapplication/MyService.java
+++ app/src/main/java/com/devedroy/serviceapplication/MyService.java
@@ -2,29 +2,74 @@ package com.devedroy.serviceapplication;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
 
 import androidx.annotation.Nullable;
 
+import java.util.Random;
+
 public class MyService extends Service {
+
+    private int mRandomNumber;
+    private boolean mIsRandomNumberGeneratorOn;
+
+    private final int MIN = 0;
+    private final int MAX = 100;
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.i("MyService", "in onStartCommand " + Thread.currentThread().getId());
-        return super.onStartCommand(intent, flags, startId);
+        mIsRandomNumberGeneratorOn = true;
+        new Thread(() -> {
+            startRandomNumberGenerator();
+        }).start();
+
+        //see flags
+        return START_STICKY;
     }
+
+    class MyServiceBinder extends Binder {
+        public MyService getService() {
+            return MyService.this;
+        }
+    }
+
+    private IBinder mIBinder = new MyServiceBinder();
 
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
-        return null;
+        return mIBinder;
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        stopRandomNumberGenerator();
         Log.i("MyService", "Service destroyed.");
+    }
 
+    private void startRandomNumberGenerator() {
+        while (mIsRandomNumberGeneratorOn) {
+            try {
+                Thread.sleep(1000);
+                if (mIsRandomNumberGeneratorOn) {
+                    mRandomNumber = new Random().nextInt(MAX) + MIN;
+                    Log.i("MyService", "in onStartCommand " + Thread.currentThread().getId() + "Random number" + mRandomNumber);
+                }
+            } catch (InterruptedException e) {
+                Log.i("MyService", "Thread Interrupted");
+            }
+        }
+    }
+
+    private void stopRandomNumberGenerator() {
+        mIsRandomNumberGeneratorOn = false;
+    }
+
+    public int getRandomNumber() {
+        return mRandomNumber;
     }
 }
